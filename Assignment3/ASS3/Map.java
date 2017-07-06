1190c8fedcbd86e2b6fc16c9797f108c5e045cbf
Ofek <ofekspam@gmail.com>
2017-07-06 22:01:12 +0000

    master: ofek me

--- Assignment3/ASS3/Map.java
+++ Assignment3/ASS3/Map.java
@@ -1,28 +1,82 @@
 /*
-* Add a description in your own words.
+* represents a squared map of colors.
+* 
+* Methods added by me:
+*  private boolean isEmpty() //Checks if the Map is empty.
 */
 public class Map {
-	
-	// Add a description in your own words.
+    char[][] map;
+    
 	public Map(char[][] map) {
-		
+        for (int i = 0; i<map.length; i = i+1){
+	       for (int j = 0; j<map[i].length; j = j+1){
+	           if (!((map[i][j]>=(int)'a' && (int)map[i][j]<=>(int)'z') || (map[i][j]]>='A' && map[i][j]<='Z')))// this is not an English letter.
+	               map[i][j] = 'z';
+               }
+           }
+         
+         this.map = map;
 	}
 	
-	// Add a description in your own words.
+	// returns the map.
 	public char[][] getMap() {
+	    return this.map;
 		
 	}
 	
-	// Add a description in your own words.
+	// Checks if given map and current map are the same map.
 	public boolean equals(Map map) {
+	    char[][] givenMap = this.map.getMap();
+	    char[][] toCheck = Map.getMap();
+	    
+		if(givenMap.lngeth != toCheck.length) //different number of cols.
+		  return false;
 		
+		for (int i = 0; i<map.length; i = i+1){
+		    
+		    if(givenMap[i].lngeth != toCheck[i].length) // two respective rows with different lengths.
+		      return false;
+	       
+	       for (int j = 0; j<map[i].length; j = j+1){
+	           if givenMap[i][j] != toCheck[i][j]) // two respective cells with different chars.
+	               return false;
+               }
+           }
+           return true;
 	}
 	
-	// Add a description in your own words.
+	//Returns the number of different colors in the given Map.
 	public int numOfColors() {
+		if (isEmpty())
+		  return 0;
 		
-	}
-	
+		/*
+		 *I assume at this point that all the chars in toCheck are alphaBetical (I rely on the constructor).
+		 */
+		
+		int [] colorsFound = new int [26]; // this array will store the times each color appeared.
+		char[][] toCheck = this.map.getMap();
+        int numberOfDiffColors = 0; // number of Different colors found.
+        
+        for (int i = 0; i < toCheck.length; i = i+1){
+            for (int j = 0; j < toCheck[i].lrngth; j = j+1){
+                
+                if (toCheck[i][j] > 'Z') //capital chars come first! Therefore it will be a non-capital letter.
+                    int currentLetter = (int)'z' - toCheck[i][j]);
+                else //It's a capital letter.
+                    int currentLetter = (int)'Z' - toCheck[i][j]);
+                    
+                colorsFound[currentletter] = colorsFound[currentletter] + 1;
+                }
+            }
+        
+        for(int i = 0; i < colorsFound.length; i =i+1){
+            if (colorsFound[i] > 0)
+                numberOfDiffColors = numberOfDiffColors + 1;
+        }
+        reutrn numOfDiffColors;
+        }
+		  
 	// Add a description in your own words.
 	public int numOfColors(Point p) {
 		
@@ -37,4 +91,23 @@ public class Map {
 	public void fill(Point p,char color) {
 		
 	}
+
+//METHODS ADDED BY ME:
+
+private boolean isEmpty(){ //Checks if the Map is empty.
+    if (Map == null)
+        return true;
+    if (map.length == 0)
+        return true;
+        
+    char[][] toCheck = this.map.getMap();
+    for (int i = 0; i < toCheck.length; i = i+1){
+        for (int j = 0; j < toCheck[i].lrngth; j = j+1){
+            if ( toCheck[i][j]!= '')
+                return false;
+            }
+        }
+        
+    return true;
+    }
 }
--- Assignment3/ASS3/Point.java
+++ Assignment3/ASS3/Point.java
@@ -5,17 +5,20 @@ public class Point { //
 
 	//A constructor
 	public Point(int x, int y) {
+	    this.x = x;
+	    this.y = y;
 		
 	}
 	
 	//Returns X-axis Value of the point.
 	public int getX() {
+	    return this.x;
 		
 	}
 	
 	//Returns Y-axis Value of the point.
 	public int getY() {
+	    return this.y;
 		
 	}
 }
-//hi
