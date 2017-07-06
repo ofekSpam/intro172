71aacf10983109f98b529a0a12920d5027134a55
ofekSpam <ofekspam@gmail.com>
2017-07-06 16:48:32 +0300

    Add files via upload
    Original given Java files

---
+++ Assignment3/OriginalFiles/Map.java
@@ -0,0 +1,40 @@
+/*
+* Add a description in your own words.
+*/
+public class Map {
+	
+	// Add a description in your own words.
+	public Map(char[][] map) {
+		
+	}
+	
+	// Add a description in your own words.
+	public char[][] getMap() {
+		
+	}
+	
+	// Add a description in your own words.
+	public boolean equals(Map map) {
+		
+	}
+	
+	// Add a description in your own words.
+	public int numOfColors() {
+		
+	}
+	
+	// Add a description in your own words.
+	public int numOfColors(Point p) {
+		
+	}
+	
+	// Add a description in your own words.
+	public boolean legalNeighbor(Point p1, Point p2) {
+		
+	}
+	
+	// Add a description in your own words.
+	public void fill(Point p,char color) {
+		
+	}
+}
