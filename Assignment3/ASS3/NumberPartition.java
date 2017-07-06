1c0460118165138987086980b8ce7a4848f9e156
ofekSpam <ofekspam@gmail.com>
2017-07-06 16:50:46 +0300

    Add files via upload

---
+++ Assignment3/ASS3/NumberPartition.java
@@ -0,0 +1,31 @@
+/*
+* Add a description in your own words.
+*/
+public class NumberPartition{
+
+    /*
+	* Check if a characteristic string defines a partition 
+    * of the numbers 1...n  such that:
+    * (1) both sides are of equal length
+    * (2) both sides are of equal sum
+    * (3) both sides have equal sums of squared elements.
+    * returns false if arguments are null / "incorrect"
+	*/
+    public static boolean isNumberPartition(int n, String s){
+        // Write your function here
+    }
+
+
+	// Add a description in your own words.
+    public static void numberPartition(int n){
+		if (n > 0) {
+			numberPartition(/*fill arguments here*/);
+		}
+    }
+	
+	// Add a description in your own words.
+    private static void numberPartition(int n /*fill additional parameters here*/){
+        // Write your recursive function here
+    }
+
+}
---
+++ Assignment3/ASS3/Point.java
@@ -0,0 +1,20 @@
+/*
+* Add a description in your own words.
+*/
+public class Point {
+
+	// Add a description in your own words.
+	public Point(int x, int y) {
+		
+	}
+	
+	// Add a description in your own words.
+	public int getX() {
+		
+	}
+	
+	// Add a description in your own words.
+	public int getY() {
+		
+	}
+}
